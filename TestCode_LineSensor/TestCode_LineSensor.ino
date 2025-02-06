030165adcd2c77de1e2d123c17f0ba9e603461e8
thesquidgrid <skbuchman23@gmail.com>
2025-02-06 16:04:51 -0500

    Create TestCode_LineSensor.ino

---
+++ TestCode_LineSensor/TestCode_LineSensor.ino
@@ -0,0 +1,43 @@
+#include "Adafruit_VL53L0X.h"
+#include <Wire.h>  // Include the Wire library for custom I2C pins
+
+Adafruit_VL53L0X lox = Adafruit_VL53L0X();
+
+void setup() {
+  Serial.begin(115200);
+
+  // wait until serial port opens for native USB devices
+  while (!Serial) {
+    delay(1);
+  }
+
+  // Initialize I2C with custom pins (SDA=16, SCL=17)
+  // This function is for Teensy, where you initialize with Wire for custom pins
+  Wire1.setSDA(16);  // Set SDA to Pin 16
+  Wire1.setSCL(17);  // Set SCL to Pin 17
+  Wire1.begin();     // Begin I2C communication on Wire1 with the custom SDA/SCL pins
+
+  Serial.println("Adafruit VL53L0X test");
+  if (!lox.begin()) {
+    Serial.println(F("Failed to boot VL53L0X"));
+    while(1);
+  }
+  // power 
+  Serial.println(F("VL53L0X API Simple Ranging example\n\n")); 
+}
+
+void loop() {
+  VL53L0X_RangingMeasurementData_t measure;
+
+  Serial.print("Reading a measurement... ");
+  lox.rangingTest(&measure, false); // pass in 'true' to get debug data printout!
+
+  if (measure.RangeStatus != 4) {  // phase failures have incorrect data
+    Serial.print("Distance (mm): ");
+    Serial.println(measure.RangeMilliMeter);
+  } else {
+    Serial.println(" out of range ");
+  }
+
+  delay(100);
+}
