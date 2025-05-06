eaa79c71ab0375816a88a702b77c18ecce5e28eb
thesquidgrid <skbuchman23@gmail.com>
2025-05-06 16:03:18 -0400

    lazy susan code added

--- ls-motor/ls-motor.ino
+++ ls-motor/ls-motor.ino
@@ -1,64 +1,42 @@
-/******************************************************************************
-TestRun.ino
-TB6612FNG H-Bridge Motor Driver Example code
-Michelle @ SparkFun Electronics
-8/20/16
-https://github.com/sparkfun/SparkFun_TB6612FNG_Arduino_
-   
-   //Use of brake again.
-   brake(motor1, motor2);
-   delay(1000);Library
-
-Uses 2 motors to show examples of the functions in the library.  This causes
-a robot to do a little 'jig'.  Each movement has an equal and opposite movement
-so assuming your motors are balanced the bot should end up at the same place it
-started.
-
-Resources:
-TB6612 SparkFun Library
-
-Development environment specifics:
-Developed on Arduino 1.6.4
-Developed with ROB-9457
-******************************************************************************/
-
-// This is the library for the TB6612 that contains the class Motor and all the
-// functions
 #include "SparkFun_TB6612.h"
+#include <Encoder.h>
 
-// Pins for all inputs, keep in mind the PWM defines must be on PWM pins
-// the default pins listed are the ones used on the Redbot (ROB-12097) with
-// the exception of STBY which the Redbot controls with a physical switch
+// Motor control pins
 #define AIN1 2
 #define AIN2 3
 #define PWMA 4
-#define STBY 9 // doesn't matter lol, is pulled high.
+#define STBY 9
 
-// these constants are used to allow you to make your motor configuration 
-// line up with function names like forward.  Value can be 1 or -1
+// Motor direction offset
 const int offsetA = 1;
-const int offsetB = 1;
 
-// Initializing motors.  The library will allow you to initialize as many
-// motors as you have memory for.  If you are using functions like forward
-// that take 2 motors as arguements you can either write new functions or
-// call the function more than once.
 Motor motor1 = Motor(AIN1, AIN2, PWMA, offsetA, STBY);
+Encoder myEnc(5, 6);  // Attach encoder to pins 5 and 6
 
-void setup()
-{
- //Nothing here
+// Target encoder displacement for 90-degree rotation
+const long targetDisplacement = 123;  
+long oldPosition = -999;
+
+void setup() {
+  Serial.begin(9600);
+  myEnc.write(0); // Reset encoder position to 0 at startup
 }
 
+void loop() {
+  motor1.drive(50); // Drive motor forward at speed 50 (no timeout)
 
-void loop()
-{
-   //Use of the drive function which takes as arguements the speed
-   //and optional duration.  A negative speed will cause it to go
-   //backwards.  Speed can be from -255 to 255.  Also use of the 
-   //brake function which takes no arguements.
-   motor1.drive(255,1000);
-   motor1.drive(-255,1000);
-   motor1.brake();
-   delay(1000);
+  long newPosition = myEnc.read();
+  
+  if (newPosition != oldPosition) {
+    oldPosition = newPosition;
+    Serial.println(newPosition); 
+  }
+
+  if (abs(newPosition) >= targetDisplacement) {
+    motor1.brake(); // Stop motor
+    Serial.println("Reached 90 degrees!");
+    myEnc.write(0); // Reset encoder position to 0 at startup
+    delay(1000);
+    newPosition = 0;
+  }
 }
